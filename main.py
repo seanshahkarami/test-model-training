312be9662651daba544e11f66890b60501aa8ea2
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-07-05 15:28:24 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import torch
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+from torchvision.models import resnet18, resnet34, resnet50, resnet101
+import time
+
+
+def test_model_epoch(modelfn):
+    if torch.has_cuda:
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    model = modelfn(pretrained=True).to(device)
+    model.train()
+
+    optimizer = SGD(model.parameters(), lr=1e-3)
+    optimizer.zero_grad()
+
+    lossfunc = CrossEntropyLoss()
+
+    input = torch.randn((4, 3, 512, 512), device=device)
+    target = torch.randint(0, 999, (len(input),), device=device)
+
+    start = time.time()
+
+    loss = lossfunc(model(input), target)
+    loss.backward()
+
+    duration = time.time() - start
+    print(modelfn.__name__, device, duration, loss.item())
+
+
+def main():
+    for modelfn in [resnet18, resnet34, resnet50, resnet101]:
+        test_model_epoch(modelfn)
+
+
+if __name__ == "__main__":
+    main()
